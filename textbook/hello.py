d054f97a40ec6faced159012e38e6f6738058fbc
Floria Fang Zhang <floria.fang9@gmail.com>
2024-09-22 21:38:55 -0400

    Add web forms for name and email.

--- textbook/hello.py
+++ textbook/hello.py
@@ -1,15 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, validators
+
+    
+def email_check(form, field):
+    if '@' not in field.data:
+        raise validators.ValidationError('Must be a valid email (must contain "@").')
+    
+class UserForm(FlaskForm):
+    name = StringField('What is your name?', validators=[validators.DataRequired()])
+    email = StringField('What is your email?', validators=[email_check])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ffz'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = UserForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+
+        new_email = form.email.data
+        if 'utoronto' in new_email:
+            session['email'] = form.email.data
+        else: 
+            session['email'] = None
+
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
